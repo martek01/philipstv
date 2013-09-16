079f97cb4736db8566d906eac6479de3a82d2914
martek01 <martek01@gmx.de>
2013-09-17 01:14:56 +0200

    ADD: Remote class for controlling the tv. POST requests are working, GET requests need to be parsed. All arguments except channellist are working.

--- src/remote.cpp
+++ src/remote.cpp
@@ -7,11 +7,186 @@
 
 #include "remote.h"
 
-Remote::Remote() {
+#include <sstream>
+#include <boost/asio.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
+boost::asio::io_service Remote::ioService;
+
+Remote::Remote() {
+	// create socket
+	socket = new boost::asio::ip::tcp::socket(ioService);
 }
 
 Remote::~Remote() {
+	// close socket connection
+	disconnect();
 
+	delete socket;
 }
 
+bool Remote::connect(boost::asio::ip::address address, unsigned short int port) {
+	// resolve address and connect to endpoint
+	boost::asio::ip::tcp::endpoint endpoint(address, port);
+	boost::system::error_code errorCode;
+
+	socket->connect(endpoint, errorCode);
+	if (errorCode) {
+		std::cout << "Could not connect to address: " << address.to_string() << ":" << port << ": " << errorCode.message() << "\n";
+		return false;
+	}
+
+	return true;
+}
+
+void Remote::disconnect() {
+	socket->shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+	socket->close();
+}
+
+boost::asio::ip::address Remote::getAddress() {
+	return socket->remote_endpoint().address();
+}
+
+unsigned short int Remote::getPort() {
+	return socket->remote_endpoint().port();
+}
+
+bool Remote::sendKey(std::string keyName) {
+	// create property tree
+	boost::property_tree::ptree propertyTree;
+	propertyTree.add("key", keyName);
+
+	return sendPostRequest("/1/input/key", propertyTree);
+}
+
+bool Remote::standby() {
+	// create property tree
+	boost::property_tree::ptree propertyTree;
+	propertyTree.add("key", "Standby");
+
+	return sendPostRequest("/1/input/key", propertyTree);
+}
+
+bool Remote::setVolume(int level) {
+	// create property tree
+	boost::property_tree::ptree propertyTree;
+	propertyTree.add("current", level);
+
+	return sendPostRequest("/1/audio/volume", propertyTree);
+}
+
+bool Remote::setChannel(std::string channelID) {
+	// create property tree
+	boost::property_tree::ptree propertyTree;
+	propertyTree.add("id", channelID);
+
+	return sendPostRequest("/1/channels/current", propertyTree);
+}
+
+bool Remote::setSource(std::string sourceName) {
+	// create property tree
+	boost::property_tree::ptree propertyTree;
+	propertyTree.add("id", sourceName);
+
+	return sendPostRequest("/1/sources/current", propertyTree);
+}
+
+std::string Remote::getChannelList() {
+	return "";
+}
+
+boost::property_tree::ptree Remote::sendGetRequest(std::string path) {
+	// create buffer stream
+	boost::asio::streambuf buffer;
+	std::ostream bufferStream(&buffer);
+
+	// add get request header
+	bufferStream << "GET " << path << " HTTP/1.1\r\n";
+	bufferStream << "Host: " << getAddress() << ":" << getPort() << "\r\n";
+	bufferStream << "Accept: */*\r\n\r\n";
+
+	// send request
+	boost::asio::write(*socket, buffer);
+
+	// get response
+	boost::asio::streambuf response;
+	boost::asio::read_until(*socket, response, "\r\n");
+
+	// validate response
+	std::istream responseStream(&response);
+	std::string httpVersion;
+	unsigned int statusCode;
+	std::string statusMessage;
+
+	responseStream >> httpVersion;
+	responseStream >> statusCode;
+	std::getline(responseStream, statusMessage);
+
+	if (!responseStream || httpVersion.substr(0, 5) != "HTTP/") {
+		std::cout << "Invalid response\n";
+		boost::property_tree::ptree emptyTree;
+		return emptyTree;
+	}
+
+	if (statusCode != 200) {
+		std::cout << "Response returned with status code: " << statusCode << "\n";
+		std::cout << statusMessage << std::endl;
+		boost::property_tree::ptree emptyTree;
+		return emptyTree;
+	}
+
+	// get response header
+	boost::asio::read_until(*socket, response, "\r\n\r\n");
+	std::cout << "Response: " << &response << std::endl;
+}
+
+bool Remote::sendPostRequest(std::string path, boost::property_tree::ptree bodyTree) {
+	// create buffer stream
+	boost::asio::streambuf buffer;
+	std::ostream bufferStream(&buffer);
+
+	// create body string from property tree
+	std::stringstream bodyStream;
+	boost::property_tree::write_json(bodyStream, bodyTree);
+	std::string body = bodyStream.str();
+
+	// add get request header
+	bufferStream << "POST " << path << " HTTP/1.1\r\n";
+	bufferStream << "Host: " << getAddress() << ":" << getPort() << "\r\n";
+	bufferStream << "Accept: */*\r\n";
+	bufferStream << "Content-Length: " << body.length() + 4 << "\r\n";
+	bufferStream << "Content-type: application/json\r\n\r\n";
+
+	bufferStream << body << "\r\n\r\n";
+
+	// send request
+	boost::asio::write(*socket, buffer);
+
+	// get response
+	boost::asio::streambuf response;
+	boost::asio::read_until(*socket, response, "\r\n");
+
+	// validate response
+	std::istream responseStream(&response);
+	std::string httpVersion;
+	unsigned int statusCode;
+	std::string statusMessage;
+
+	responseStream >> httpVersion;
+	responseStream >> statusCode;
+	std::getline(responseStream, statusMessage);
+
+	if (!responseStream || httpVersion.substr(0, 5) != "HTTP/") {
+		std::cout << "Invalid response\n";
+		return false;
+	}
+
+	if (statusCode != 200) {
+		std::cout << "Response returned with status code: " << statusCode << "\n";
+		std::cout << statusMessage << std::endl;
+		return false;
+	}
+
+	return true;
+}
--- src/remote.h
+++ src/remote.h
@@ -8,10 +8,40 @@
 #ifndef REMOTE_H_
 #define REMOTE_H_
 
+#include <string>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/address.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/property_tree/ptree.hpp>
+
 class Remote {
+protected:
+	boost::asio::ip::tcp::socket *socket;
+
+	static boost::asio::io_service ioService;
+
 public:
 	Remote();
 	virtual ~Remote();
+
+	bool connect(boost::asio::ip::address address, unsigned short int port = 1925);
+	void disconnect();
+
+	boost::asio::ip::address getAddress();
+	unsigned short int getPort();
+
+	bool sendKey(std::string keyName);
+	bool standby();
+	bool setVolume(int level);
+	bool setChannel(std::string channelID);
+	bool setSource(std::string sourceName);
+
+	std::string getChannelList();
+
+protected:
+	boost::property_tree::ptree sendGetRequest(std::string path);
+	bool sendPostRequest(std::string path, boost::property_tree::ptree bodyTree);
 };
 
 #endif /* REMOTE_H_ */
